57dbd59a6ae48a0227c3c26424dae4d25967d652
yakhyo <muhammadyahyodev@gmail.com>
2025-01-25 14:37:17 +0500

    create TCP server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const net = require('net');
 const clients = [];
 
 const server = net.createServer((socket) => {
-    console.log('Client connected');
     clients.push(socket);
+    console.log('Client connected');
 
     socket.on('data', (data) => {
         console.log('Received from client:', data.toString());
